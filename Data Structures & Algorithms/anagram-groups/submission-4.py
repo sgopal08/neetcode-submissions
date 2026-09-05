98e81f097a650d86de60f5247ba8e18907f4448c
Sanjana Gopalswamy <143037664+sgopal08@users.noreply.github.com>
2026-09-05 18:44:16 -0400

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        # whiteboarding:
+        # list for all the character frequencies
+
+        # step 1) for each word, create a list for char freqs
+        # step 2) lists of the same will be grouped together in the same list
+        # step 3) store them in a hash map: key can be the list, and the value can be a list of the words that correspond to that char freq list
+        # step 4) we can return the values as a list
+
+        word_map = defaultdict(list)
+
+
+        for word in strs:
+            count = [0] * 26
+
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+            
+            word_map[tuple(count)].append(word)
+        
+        return list(word_map.values())
+
+
+
+
+
+
+        
+        
